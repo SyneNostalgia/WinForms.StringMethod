3c2c97e62bb83f4c1b3bc146379085ef16047532
SyneNostalgia <achitathenew@gmail.com>
2024-12-11 11:45:13 +0700

    Add project files.

---
+++ WinForms.StringMethod/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WinForms.StringMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtFullname = new TextBox();
+            txtTitle = new TextBox();
+            txtFirstName = new TextBox();
+            txtLastName = new TextBox();
+            label1 = new Label();
+            btnSplitName = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // txtFullname
+            // 
+            txtFullname.Location = new Point(146, 66);
+            txtFullname.Name = "txtFullname";
+            txtFullname.Size = new Size(425, 27);
+            txtFullname.TabIndex = 0;
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(146, 133);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(240, 27);
+            txtTitle.TabIndex = 1;
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(146, 166);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(240, 27);
+            txtFirstName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(146, 199);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(240, 27);
+            txtLastName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(59, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 20);
+            label1.TabIndex = 4;
+            label1.Text = "ชื่อ-นามสกุล";
+            // 
+            // btnSplitName
+            // 
+            btnSplitName.Location = new Point(605, 66);
+            btnSplitName.Name = "btnSplitName";
+            btnSplitName.Size = new Size(94, 29);
+            btnSplitName.TabIndex = 5;
+            btnSplitName.Text = "แยกชื่อสกุล";
+            btnSplitName.UseVisualStyleBackColor = true;
+            btnSplitName.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(59, 169);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 20);
+            label2.TabIndex = 6;
+            label2.Text = "ชื่อ";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(59, 199);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 20);
+            label3.TabIndex = 7;
+            label3.Text = "นามสกุล";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(59, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 20);
+            label4.TabIndex = 8;
+            label4.Text = "คำนำหน้า";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(763, 299);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btnSplitName);
+            Controls.Add(label1);
+            Controls.Add(txtLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(txtTitle);
+            Controls.Add(txtFullname);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtFullname;
+        private TextBox txtTitle;
+        private TextBox txtFirstName;
+        private TextBox txtLastName;
+        private Label label1;
+        private Button btnSplitName;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ WinForms.StringMethod/Form1.cs
@@ -0,0 +1,34 @@
+﻿namespace WinForms.StringMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fullname= "" ,firstname = "", lastname = "", title = "";
+            int x = 0;
+            fullname = txtFullname.Text.Trim();
+            if (fullname.StartsWith("นางสาว"))
+            {
+                x = 6;
+                title = fullname.Substring(0, x);
+            }
+            else if (fullname.StartsWith("นาง") || fullname.StartsWith("นาย"))
+            {
+                x = 3;
+                title = fullname.Substring(0, x);
+            }
+            fullname = fullname.Substring(x).Trim();
+            MessageBox.Show(title + "\n" + fullname);
+        }
+    }
+}
